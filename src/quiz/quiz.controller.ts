3b43d8b4d48e33d5470d9cab0a33b40dc62dccdb
JeongHawook <saroball3@gmail.com>
2023-07-31 01:25:15 +0900

    Feat: GetQuiz method

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { GetUser } from 'src/users/common/decorators';
 import { QuizService } from './quiz.service';
 import { Quiz } from './quiz.entity';
+import { Users } from 'src/users/users.entity';
+import { CreateQuizDto } from './dtos/quiz-dtos';
 
 @Controller('quiz')
 export class QuizController {
-constructor(private quizService : QuizService){}
+    constructor(private quizService: QuizService) { }
 
 
 
-@Get('/:subject/:level')
-async getQuiz(
-    @Param('subject') subject:string,
-    @Param('level', ParseIntPipe) level: number
-):Promise<Quiz[]>{
-    const getQuiz= await this.quizService.getQuiz(subject, level)
-    return
-}
+    @Get('/:subject/:level')
+    async getQuiz(
+        @Param('subject') subject: string,
+        @Param('level', ParseIntPipe) level: number
+    ): Promise<Quiz[]> {
+        const getQuiz = await this.quizService.getQuiz(subject, level)
+        return
+    }
+
+    @Post('/:subject')
+    async createQuiz(
+        @GetUser() user: Users,
+        @Param('subject') subject: string,
+        @Body() createQuizDto: CreateQuizDto
+    ): Promise<string> {
+        //const createQuiz = await this.quizService.createQuiz(subject, createQuizDto, user)
+        return
+    }
 
 }
